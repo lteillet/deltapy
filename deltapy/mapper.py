9adcc359a5b6bcb6cc579e030100503df5b8fd06
lteillet <166124111+lteillet@users.noreply.github.com>
2025-02-05 14:56:51 +0100

    Update mapper.py

--- deltapy/mapper.py
+++ deltapy/mapper.py
@@ -7,7 +7,6 @@ from sklearn import datasets, cluster
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 
 
 def pca_feature(df, memory_issues=False,mem_iss_component=False,variance_or_components=0.80,n_components=5 ,drop_cols=None, non_linear=True):
@@ -92,44 +91,6 @@ def a_chi(df, drop=None, lags=1, sample_steps=2 ):
   return df
 
 # df_out = a_chi(df)
-
-
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-def encoder_dataset(df, drop=None, dimesions=20):
-
-  if drop:
-    train_scaled = minmax_scale(df.drop(drop,axis=1).values, axis = 0)
-  else:
-    train_scaled = minmax_scale(df.values, axis = 0)
-
-  # define the number of encoding dimensions
-  encoding_dim = dimesions
-  # define the number of features
-  ncol = train_scaled.shape[1]
-  input_dim = tf.keras.Input(shape = (ncol, ))
-
-  # Encoder Layers
-  encoded1 = tf.keras.layers.Dense(3000, activation = 'relu')(input_dim)
-  encoded2 = tf.keras.layers.Dense(2750, activation = 'relu')(encoded1)
-  encoded3 = tf.keras.layers.Dense(2500, activation = 'relu')(encoded2)
-  encoded4 = tf.keras.layers.Dense(750, activation = 'relu')(encoded3)
-  encoded5 = tf.keras.layers.Dense(500, activation = 'relu')(encoded4)
-  encoded6 = tf.keras.layers.Dense(250, activation = 'relu')(encoded5)
-  encoded7 = tf.keras.layers.Dense(encoding_dim, activation = 'relu')(encoded6)
-
-  encoder = tf.keras.Model(inputs = input_dim, outputs = encoded7)
-  encoded_input = tf.keras.Input(shape = (encoding_dim, ))
-
-  encoded_train = pd.DataFrame(encoder.predict(train_scaled),index=df.index)
-  encoded_train = encoded_train.add_prefix('encoded_')
-  if drop:
-    encoded_train = pd.concat((df[drop],encoded_train),axis=1)
-
-  return encoded_train
-
-# df_out = encoder_dataset(df, ["Close_1"], 15)
-
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
